cc69669e03d1a82d9f4f0410347bbdf68ede8762
yaremkoPavlo <yaremko.pavlo@gmail.com>
2018-05-11 12:44:44 +0300

    done extra task

--- 01/extra.php
+++ 01/extra.php
@@ -8,10 +8,35 @@
 "ZzZz", "ffPFF" -> equal
 "kl", "lz" -> not equal
 null, "" -> equal
-
  */
 
+/**
+ * @param $ch1:string
+ * @param $ch2:string
+ * @return boolean
+ */
 function compareCharacters($ch1, $ch2)
 {
+    $sum1 = getSumCharacterCode($ch1);
+    $sum2 = getSumCharacterCode($ch2);
+
+    return $sum1 === $sum2;
+}
+
+/**
+ * @param $ch:string
+ * @return int
+ */
+function getSumCharacterCode($ch)
+{
+    $sum = 0;
+    $ch = strtoupper($ch);
+    for ($i=0; $i < strlen($ch); $i++) {
+        $sum += ord($ch[$i]);
+    }
+
+    return $sum;
 
 }
+
+var_dump(compareCharacters("ZzZz", "ffPFF"));
